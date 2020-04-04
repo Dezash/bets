78ca0d63dd517e481ddf61752eafaab873ff2790
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-04-04 13:59:40 +0300

    Create enums config

---
+++ config/enums.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'payment_types' => [
+        'VISA',
+        'Maestro',
+        'MasterCard',
+        'American Express',
+        'Revolut',
+        'PayPal',
+        'Skrill'
+    ]
+];
