63b091d0d6fb9429b0b83f63d95a8568072b0c57
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-10-13 18:27:52 +0300

    Translate to English

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Sukurti darbuotoją</h1>
+    <h1>Add employee</h1>
     {{ Form::open(['action' => 'EmployeesController@store', 'method' => 'POST']) }}
         <div class="form-group">
-            {{Form::label('first_name', 'Vardas')}}
-            {{Form::text('first_name', '', ['class' => 'form-control', 'placeholder' => 'Vardas'])}}
-            {{Form::label('last_name', "Pavardė")}}
-            {{Form::text('last_name', '', ['class' => 'form-control', 'placeholder' => 'Pavardė'])}}
-            {{Form::label('birth_date', "Gimimo data")}}
+            {{Form::label('first_name', 'First name')}}
+            {{Form::text('first_name', '', ['class' => 'form-control', 'placeholder' => 'First name'])}}
+            {{Form::label('last_name', "Last name")}}
+            {{Form::text('last_name', '', ['class' => 'form-control', 'placeholder' => 'Last name'])}}
+            {{Form::label('birth_date', "Birht date")}}
             {{Form::date('birth_date', '', ['class' => 'form-control'])}}
         </div>
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
